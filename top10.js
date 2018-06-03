6273eda8bbc87a715373aa652e07fd3b9f00f508
Nilesh <nil@gmail.com>
2018-06-03 11:02:18 +0530

    top10 completed

--- top10.js
+++ top10.js
@@ -1,7 +1,16 @@
-function topTen(){
-	function reqListener (){
- 			var a = JSON.parse(this.responseText);
-			console.log(a);
+	function topTen(){
+		function reqListener (){
+			document.getElementById("demo").innerHTML=" ";
+ 			var xyz = JSON.parse(this.responseText);
+ 			
+ 			console.log(xyz);
+			for(var i=0;i<10;i++)
+			{
+				var a=xyz["track"];
+				var o=a[i];
+				if( o.strMusicVid!=null && o.strDescriptionEN!=null )
+				document.getElementById("demo").innerHTML  = document.getElementById("demo").innerHTML + "Track : "+ o.strTrack+"<br/>"+ "Album : "+ o.strAlbum + "<br/>" + "Genre : "+ o.strGenre + "<br/>" +"Youtube Video Link : " + o.strMusicVid + "<br/><br/>"+" Description :" +"<br/>"+ o.strDescriptionEN + "<br/><br/><br/>";
+			}
 		}
 
 		var var1=document.getElementById("artist").value;
@@ -11,7 +20,5 @@ oReq.addEventListener("load", reqListener);
 oReq.open("GET", `http://www.theaudiodb.com/api/v1/json/195003/track-top10.php?s=${var1}`);
 oReq.send();
 
-
-
-
 }
+
